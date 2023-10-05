1407bad6f315ee31eeb93680e5df4025b3fd3619
Aleksey <alekseylebedev322@gmail.com>
2023-10-05 23:01:41 +0300

    added modalWindow + form

---
+++ src/components/layout/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import "../ModalWindow/ModalWindow.scss";
+interface formTypes {
+  name: string;
+  phone: string;
+  description: string;
+}
+const ModalWindow = () => {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [description, setDescription] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<formTypes>();
+  const handler = () => {
+    let modal = document.getElementById("modal");
+    modal?.classList.remove("active");
+  };
+  const onSubmit: SubmitHandler<formTypes> = () => {
+    let modal = document.getElementById("modal");
+    modal?.classList.remove("active");
+    reset({
+        phone: '',
+        name: '',
+        description: '',
+    })
+  };
+  return (
+    <div
+      id='modal'
+      className='top-0 bottom-0 right-0 left-0 fixed hidden authModal justify-center items-center z-[900]'
+      onClick={handler}
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className='imgBg w-[550px] h-[630px] rounded-[10px] z-[99999999] max-mm:w-[400px] max-xsml:w-[280px] max-xsml:h-[550px]'
+      >
+        <div className='flex flex-col justify-start items-start w-full h-full pl-[30px] pt-[20px]'>
+            <span className="modalTitle">Обратная форма</span>
+          <div className="w-full">
+            <p className='desc'>Фио</p>
+            <input
+              type='name'
+              className='base-input'
+              placeholder='Иванов Иван Иванович'
+              {...register("name", {
+                required: "Введите Фио",
+              })}
+              onChange={e => setName(e.target.value)}
+            />
+            {errors?.name && <div className='error'>{errors.name.message}</div>}
+          </div>
+          <div className='mt-[20px] w-full'>
+            <p className='desc'>Номер телефона</p>
+            <input
+              type='phoneNumber'
+              className='base-input'
+              placeholder='+7 915 422 91 11'
+              {...register("phone", {
+                required: "Введите номер телефона",
+                pattern: {
+                  value: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
+                  message: "Введите корректный номер",
+                },
+              })}
+              onChange={e => setPhoneNumber(e.target.value)}
+            />
+            {errors?.phone && (
+              <div className='error'>{errors.phone.message}</div>
+            )}
+          </div>
+          <div className='mt-[20px] w-full'>
+            <p className='desc'>Опишите ваш кейс</p>
+            <textarea
+              className='base-area'
+              placeholder='Опишите какой сайт вам нужен, его функционал, внешний вид и принадлежность'
+              {...register("description", {
+                required:
+                  "Опишите, как должен выглядить проек, это важно для нас",
+              })}
+              onChange={e => setDescription(e.target.value)}
+            />
+            {errors?.description && (
+              <div className='error'>{errors.description.message}</div>
+            )}
+          </div>
+          <button className='mt-[20px] border px-[40px] py-[15px] rounded-[4px] hover:bg-white hover:text-[#1b1b1b] max-xsml:text-[12px] w-[93%]' onClick={handleSubmit(onSubmit)}>
+            <p>Отправить форму</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWindow;
--- src/components/sections/home/Contacts/Contact.tsx
+++ src/components/sections/home/Contacts/Contact.tsx
@@ -1,34 +1,59 @@
+"use client";
 import cl from "./Contact.module.scss";
-
+import React from "react";
+import "@/components/layout/ModalWindow/ModalWindow.scss";
+import ModalWindow from "@/components/layout/ModalWindow/ModalWindow";
 const Contact = () => {
+  const openModal = () => {
+    const modal = document.getElementById("modal");
+    modal?.classList.add("active");
+  };
   return (
-    <div className='container pt-10 flex flex-col justify-center items-center mt-[100px] mb-[50px]'>
+    <div className='container pt-10 flex flex-col justify-center items-center mt-[100px] mb-[50px] relative'>
+      <ModalWindow />
       <h2 className={cl.title}>
         <span className={cl.main}>contact with us</span>
-        <span className='absolute top-[-50px] left-[140px] max-l:text-[80px] max-l:left-[100px] max-ipad:text-[65px] max-ipad:top-[-35px] max-ipad:left-[100px] max-mm:text-[40px] max-mm:top-[-15px] max-sml:left-[60px] max-xsml:text-[27px] max-ssml:left-[30px]'>связь с нами</span>
+        <span className='absolute top-[-50px] left-[140px] max-l:text-[80px] max-l:left-[100px] max-ipad:text-[65px] max-ipad:top-[-35px] max-ipad:left-[100px] max-mm:text-[40px] max-mm:top-[-15px] max-sml:left-[60px] max-xsml:text-[27px] max-ssml:left-[30px]'>
+          связь с нами
+        </span>
       </h2>
       <div className='flex flex-row justify-between items-center w-[70%] h-[200px] max-xsml:flex-col max-xsml:h-[140px] max-xsml:w-full'>
         <button className='border-[2px] rounded-[70px] bg-none w-[450px] h-[100px] max-l:w-[320px] max-s:w-[270px] max-med:w-[180px] max-med:h-[50px] max-xsml:w-[280px] hover:shadow-md hover:bg-white hover:text-[#191919] duration-300'>
           <span className={cl.buttons}>Написать нам</span>
         </button>
-        <button className='border-[2px] rounded-[70px] bg-none w-[450px] h-[100px] max-l:w-[320px] max-s:w-[270px] max-med:w-[180px] max-med:h-[50px] max-xsml:w-[280px] hover:shadow-md hover:bg-white hover:text-[#191919] duration-300'>
+        <button
+          onClick={openModal}
+          className='border-[2px] rounded-[70px] bg-none w-[450px] h-[100px] max-l:w-[320px] max-s:w-[270px] max-med:w-[180px] max-med:h-[50px] max-xsml:w-[280px] hover:shadow-md hover:bg-white hover:text-[#191919] duration-300'
+        >
           <span className={cl.buttons}>Обсудить проект</span>
         </button>
       </div>
       <div className='flex flex-row justify-between items-center w-[80%] h-[200px] max-xsml:w-[95%]'>
-        <a href="https://t.me/knkweb" className='flex flex-col w-[20%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'>
+        <a
+          href='https://t.me/knkweb'
+          className='flex flex-col w-[20%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'
+        >
           <span className={cl.socials}>Telegram</span>
           <span className={cl.link}>@idntnow</span>
         </a>
-        <a href="https://www.youtube.com/@knkwebstudio" className='flex flex-col w-[15%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'>
+        <a
+          href='https://www.youtube.com/@knkwebstudio'
+          className='flex flex-col w-[15%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'
+        >
           <span className={cl.socials}>YouTube</span>
           <span className={cl.link}>knkweb.</span>
         </a>
-        <a href="mailto:kantorin.ilya@yandex.ru" className='flex flex-col w-[25%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'>
+        <a
+          href='mailto:kantorin.ilya@yandex.ru'
+          className='flex flex-col w-[25%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'
+        >
           <span className={cl.socials}>Gmail</span>
           <span className={cl.link}>kantorin.ilya@yandex.ru</span>
         </a>
-        <a href="tel:+79144054140" className='flex flex-col w-[20%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'>
+        <a
+          href='tel:+79144054140'
+          className='flex flex-col w-[20%] justify-center items-start border-t-[2px] pt-[40px] max-xsml:pt-[20px] hover:underline'
+        >
           <span className={cl.socials}>Phone</span>
           <span className={cl.link}>+79144054140</span>
         </a>
