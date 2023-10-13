a97257422841dac1ae36578e4dd0daf3fc581435
Aleksey <alekseylebedev322@gmail.com>
2023-10-13 13:26:06 +0300

    animation fix

--- src/components/sections/home/OurServices/OurServices.tsx
+++ src/components/sections/home/OurServices/OurServices.tsx
@@ -15,10 +15,10 @@ const OurServices = () => {
     })
   }
   return (
-    <motion.div initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.3}} className='mb-40 container flex flex-col items-center'>
+    <motion.div initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.15}} className='mb-40 container flex flex-col items-center'>
       <div className='flex items-center gap-20 max-med:gap-10 max-ssml:gap-4 max-ssml:mb-[20px] max-xsml:gap-[25px]'>
-        <motion.span custom={1} variants={textAnimation} className={cl.spectr}>СПЕКТР</motion.span>
-        <motion.h2 custom={1} variants={textAnimation} className='text-[160px] font-semibold flex-[50%] max-s:text-[120px] max-med:text-[90px] max-xsml:text-[70px] max-ssml:text-[45px]'>УСЛУГ:</motion.h2>
+        <motion.span custom={3} variants={textAnimation} className={cl.spectr}>СПЕКТР</motion.span>
+        <motion.h2 custom={3} variants={textAnimation} className='text-[160px] font-semibold flex-[50%] max-s:text-[120px] max-med:text-[90px] max-xsml:text-[70px] max-ssml:text-[45px]'>УСЛУГ:</motion.h2>
       </div>
       <div className='flex w-full justify-between mb-10 max-sml:flex-col'>
         <div className='flex flex-col w-[49%] self-start p-8 border-2 border-white rounded-[40px] relative z-10 max-med:h-full max-sml:w-full max-sml:mb-10'>
