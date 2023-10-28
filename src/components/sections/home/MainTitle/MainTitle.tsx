cf0195fec3e63c6ce79cfb5688cb52b1279a3eeb
Aleksey <alekseylebedev322@gmail.com>
2023-10-28 11:47:45 +0300

    +mobile

--- src/components/sections/home/MainTitle/MainTitle.tsx
+++ src/components/sections/home/MainTitle/MainTitle.tsx
@@ -9,14 +9,25 @@ const Title = () => {
     if(screenY == 0){
       document.getElementById("main")?.classList.add('noScroll')
     }
-      window.addEventListener('wheel', () => {
+    if(window.screen.width < 1024){
+      window.addEventListener('scroll', () => {
         setIsVisible(true)
-        setTimeout(() => setIsVisible2(true), 1170)
+        setTimeout(() => setIsVisible2(true), 670)
         setTimeout(() =>{
           document.getElementById('needHide')?.classList.remove('hide')
           document.getElementById("main")?.classList.remove('noScroll')
-        }, 1350)
+        }, 1250)
       })
+    }else{
+      window.addEventListener('wheel', () => {
+        setIsVisible(true)
+        setTimeout(() => setIsVisible2(true), 670)
+        setTimeout(() =>{
+          document.getElementById('needHide')?.classList.remove('hide')
+          document.getElementById("main")?.classList.remove('noScroll')
+        }, 1250)
+      })
+    }
   }, [])
   
   return (
@@ -27,14 +38,14 @@ const Title = () => {
       <h2 className='lg:text-[50px] sm:text-[40px] text-[25px] sub-logo'>Современная веб студия</h2>
     <AnimatePresence>
       { isVisible && (
-      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 1}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-white overflow-hidden bottom-0'>
+      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 0.6}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-white overflow-hidden bottom-0'>
       </motion.div>
       )
       }
     </AnimatePresence>
     <AnimatePresence>
       { isVisible2 && (
-      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 1}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-[#191919] overflow-hidden bottom-0 z-10 baz'>
+      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 0.5}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-[#191919] overflow-hidden bottom-0 z-10 baz'>
         <h1 id='needHide' className='main-logo lg:text-[100px] sm:text-[70px] text-[50px] mb-4 font-bold hide'>самолет себе хочу</h1>
       </motion.div>
       )
