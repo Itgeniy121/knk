a7311fa5d6472bc32a510931113d1e9003c57694
Aleksey <alekseylebedev322@gmail.com>
2023-10-28 22:27:13 +0300

    fix

--- src/components/sections/home/MainTitle/MainTitle.tsx
+++ src/components/sections/home/MainTitle/MainTitle.tsx
@@ -1,24 +1,17 @@
 'use client'
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import './MainTitle.scss'
+import { useInView } from 'react-intersection-observer';
 import { AnimatePresence, motion } from 'framer-motion';
 const Title = () => {
   const[isVisible, setIsVisible] = useState(false)
   const[isVisible2, setIsVisible2] = useState(false)
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+  });
   useLayoutEffect(() =>{
-    if(screenY == 0){
+    if(window.screenY === 0 && typeof window !== undefined && window.pageYOffset == 0){
       document.getElementById("main")?.classList.add('noScroll')
-    }
-    if(window.screen.width < 1024){
-      window.addEventListener('scroll', () => {
-        setIsVisible(true)
-        setTimeout(() => setIsVisible2(true), 670)
-        setTimeout(() =>{
-          document.getElementById('needHide')?.classList.remove('hide')
-          document.getElementById("main")?.classList.remove('noScroll')
-        }, 1250)
-      })
-    }else{
       window.addEventListener('wheel', () => {
         setIsVisible(true)
         setTimeout(() => setIsVisible2(true), 670)
@@ -29,11 +22,25 @@ const Title = () => {
       })
     }
   }, [])
-  
+  useEffect(()=>{
+    if(inView == true){
+      document.getElementById("main")?.classList.add('noScroll')
+      window.addEventListener('wheel', () => {
+        setTimeout(() => setIsVisible(true), 1000)
+        setTimeout(() => setIsVisible2(true), 1670)
+        setTimeout(() =>{
+          document.getElementById('needHide')?.classList.remove('hide')
+          document.getElementById("main")?.classList.remove('noScroll')
+        }, 2250)
+      })
+    }
+  }, [inView])
   return (
     <div
       className='w-screen h-screen flex-center flex-col items-center justify-center flex relative'
     >
+      <div className='w-[1px] h-[1px] absolute top-0 bg-black' ref={ref}>
+       </div>
       <h1 className='main-logo lg:text-[100px] sm:text-[70px] text-[50px] mb-4 font-bold'>knk web.</h1>
       <h2 className='lg:text-[50px] sm:text-[40px] text-[25px] sub-logo'>Современная веб студия</h2>
     <AnimatePresence>
@@ -45,8 +52,8 @@ const Title = () => {
     </AnimatePresence>
     <AnimatePresence>
       { isVisible2 && (
-      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 0.5}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-[#191919] overflow-hidden bottom-0 z-10 baz'>
-        <h1 id='needHide' className='main-logo lg:text-[100px] sm:text-[70px] text-[50px] mb-4 font-bold hide'>самолет себе хочу</h1>
+      <motion.div initial={{height: 0}} animate={{height: '100vh'}} transition={{duration: 0.7}} exit={{y: '-100vh'}} className='flex justify-center items-center absolute w-full h-full bg-[#191919] overflow-hidden bottom-0 z-10 baz'>
+        <h1 id='needHide' className='main-logo2 lg:text-[100px] sm:text-[70px] text-[50px] mb-4 font-bold hide'>самолет себе хочу</h1>
       </motion.div>
       )
       }
